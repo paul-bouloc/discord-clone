2623061a3a1dc516515e6d553390d76122c157aa
moicestpaul <bouloc.p@gmail.com>
2024-09-28 23:50:48 +0200

    Added .env validation

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,22 @@
+
+import * as dotenv from 'dotenv'
+import http from 'http';
+import validateEnv from './utils/validate-env.util';
 import app from './app';
+import serverErrorHandler from './utils/server-error-handler.util';
 
-const port = 3000;
+dotenv.config();
+validateEnv();
 
-app.listen(port, () => {
-  console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
-});
+const PORT = process.env.PORT || 3000;
+
+app.set('port', PORT);
+
+const server = http.createServer(app);
+
+server.on('error', serverErrorHandler);
+server.on('listening', () => {
+  console.log(`Server listening on port ${PORT}`);
+})
+
+server.listen(PORT);
