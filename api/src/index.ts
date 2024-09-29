e0d1576ded2e072142b2b240ed3a06bd84281d5d
moicestpaul <bouloc.p@gmail.com>
2024-09-29 03:33:19 +0200

    Added prisma schema & client

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,6 +4,8 @@ import * as dotenv from 'dotenv';
 import http from 'http';
 import app from './app';
 import { serverErrorHandler, validateEnv } from '@/utils';
+import PrismaService from '@/services/prisma.service';
+import { Prisma } from '@prisma/client';
 
 dotenv.config();
 validateEnv();
@@ -13,6 +15,11 @@ const PORT = process.env.PORT || 3000;
 app.set('port', PORT)
 
 const server = http.createServer(app);
+const prisma = PrismaService.getInstance();
+
+prisma.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch((error) => console.error('Error connecting to the database:', error));
 
 server.on('error', serverErrorHandler);
 server.on('listening', () => {
@@ -20,3 +27,8 @@ server.on('listening', () => {
 })
 
 server.listen(PORT);
+
+process.on('SIGINT', async () => {
+  await prisma.disconnect();
+  process.exit(0);
+});
